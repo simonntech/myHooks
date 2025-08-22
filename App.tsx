76a989ca0d6b764f025105f00b6619aebae3c6cd
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-08-22 19:43:32 -0300

    init and no hooks

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  let quantity: number = 12;
+
+  const removeNumber = () => {
+    quantity = quantity - 1;
+    console.log(quantity)
+  }; 
+
+  const addNumber = () => {
+    quantity = quantity + 1;
+    console.log(quantity)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+
+      <View style={styles.btnRow}>
+        <Button title='-' onPress={removeNumber}/>
+        <Text style={styles.txtLbl}>{quantity}</Text>
+        <Button title='+' onPress={addNumber}/>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +36,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  txtLbl: {
+    fontSize: 22
+  },
+  btnRow: {
+    flexDirection: 'row',
+    gap: 10
+  }
 });
